cd8c2583dfd8e11ae799203d69a24c097ede03c8
Dmitry Chestnykh <dmitry@codingrobots.com>
2014-01-05 14:14:36 +0100

    Initial pages commit

---
+++ nacl.js
@@ -0,0 +1,310 @@
+(function(exports) {
+  'use strict';
+
+/* XSalsa20 */
+
+function L32(x, c) { return (x << c) | (x >>> (32 - c)); }
+
+function ld32(x, pos) {
+  var u = x[pos+3];
+  u = (u<<8)|x[pos+2];
+  u = (u<<8)|x[pos+1];
+  return (u<<8)|x[pos+0];
+}
+
+function st32(x, xpos, u) {
+  var i;
+  for(i = 0; i < 4; i++) { x[xpos+i] = u & 255; u >>>= 8; }
+}
+
+function core(out,inp,k,c,h) {
+  var w = [], x = [], y = [], t = [];
+  var i, j, m;
+
+  for(i = 0; i < 4; i++) {
+    x[5*i] = ld32(c, 4*i);
+    x[1+i] = ld32(k, 4*i);
+    x[6+i] = ld32(inp, 4*i);
+    x[11+i] = ld32(k, 16+4*i);
+  }
+
+  for(i = 0; i < 16; i++) y[i] = x[i];
+
+  for(i = 0; i < 20; i++) {
+    for(j = 0; j < 4; j++) {
+      for(m = 0; m < 4; m++) t[m] = x[(5*j+4*m)%16];
+      t[1] ^= L32((t[0]+t[3])|0, 7);
+      t[2] ^= L32((t[1]+t[0])|0, 9);
+      t[3] ^= L32((t[2]+t[1])|0,13);
+      t[0] ^= L32((t[3]+t[2])|0,18);
+      for(m = 0; m < 4; m++) w[4*j+(j+m)%4] = t[m];
+    }
+    for(m = 0; m < 16; m++) x[m] = w[m];
+  }
+
+  if (h) {
+    for(i = 0; i < 16; i++) x[i] = (x[i] + y[i]) | 0;
+    for(i = 0; i < 4; i++) {
+      x[5*i] = (x[5*i] - ld32(c, 4*i)) | 0;
+      x[6+i] = (x[6+i] - ld32(inp, 4*i)) | 0;
+    }
+    for(i = 0; i < 4; i++) {
+      st32(out,4*i,x[5*i]);
+      st32(out,16+4*i,x[6+i]);
+    }
+  } else {
+    for(i = 0; i < 16; i++) st32(out, 4 * i, (x[i] + y[i]) | 0);
+  }
+}
+
+function crypto_core_salsa20(out,inp,k,c) {
+  core(out,inp,k,c,false);
+}
+
+function crypto_core_hsalsa20(out,inp,k,c) {
+  core(out,inp,k,c,true);
+}
+
+var sigma = [101, 120, 112, 97, 110, 100, 32, 51, 50, 45, 98, 121, 116, 101, 32, 107];
+            // "expand 32-byte k"
+
+function crypto_stream_salsa20_xor(c,cpos,m,mpos,b,n,k) {
+  var z = [], x = [];
+  var u, i;
+  if (!b) return;
+  for(i = 0; i < 16; i++) z[i] = 0;
+  for(i = 0; i < 8; i++) z[i] = n[i];
+  while (b >= 64) {
+    crypto_core_salsa20(x,z,k,sigma);
+    for(i = 0; i < 64; i++) c[cpos+i] = (m?m[mpos+i]:0) ^ x[i];
+    u = 1;
+    for (i = 8;i < 16;++i) {
+      u = u + z[i] | 0;
+      z[i] = u;
+      u >>>= 8;
+    }
+    b -= 64;
+    cpos += 64;
+    if (m) mpos += 64;
+  }
+  if (b > 0) {
+    crypto_core_salsa20(x,z,k,sigma);
+    for(i = 0; i < b; i++) c[cpos+i] = (m?m[mpos+i]:0) ^ x[i];
+  }
+}
+
+function crypto_stream_salsa20(c,cpos,d,n,k) {
+  crypto_stream_salsa20_xor(c,cpos,null,0,d,n,k);
+}
+
+function crypto_stream(c,cpos,d,n,k) {
+  var i, s = [], subn = [];
+  crypto_core_hsalsa20(s,n,k,sigma);
+  for(i = 0; i < 8; i++) subn[i] = n[16+i];
+  crypto_stream_salsa20(c,cpos,d,subn,s);
+}
+
+function crypto_stream_xor(c,cpos,m,mpos,d,n,k) {
+  var i, s = [], subn = [];
+  crypto_core_hsalsa20(s,n,k,sigma);
+  for(i = 0; i < 8; i++) subn[i] = n[16+i];
+  crypto_stream_salsa20_xor(c,cpos,m,mpos,d,subn,s);
+}
+
+/* Poly1305 */
+
+function crypto_onetimeauth(out, outpos, m, mpos, n, k) {
+  var add1305 = function(h, c) {
+    var j, u = 0;
+    for(j = 0; j < 17; j++) {
+      u = (u + ((h[j] + c[j]) | 0)) | 0;
+      h[j] = u & 255;
+      u >>>= 8;
+    }
+  }
+
+  var minusp = [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 252];
+
+  var s, i, j, u ;
+  var x = [], r = [], h = [], c = [], g = [];
+  for(j = 0; j < 17; j++) r[j]=h[j]=0;
+  for(j = 0; j < 16; j++) r[j]=k[j];
+  r[3]&=15;
+  r[4]&=252;
+  r[7]&=15;
+  r[8]&=252;
+  r[11]&=15;
+  r[12]&=252;
+  r[15]&=15;
+
+  while (n > 0) {
+    for(j = 0; j < 17; j++) c[j] = 0;
+    for (j = 0;(j < 16) && (j < n);++j) c[j] = m[mpos+j];
+    c[j] = 1;
+    mpos += j; n -= j;
+    add1305(h,c);
+    for(i = 0; i < 17; i++) {
+      x[i] = 0;
+      for(j = 0; j < 17; j++) x[i] += (h[j] * ((j <= i) ? r[i - j] : ((320 * r[i + 17 - j])|0))) | 0;
+    }
+    for(i = 0; i < 17; i++) h[i] = x[i];
+    u = 0;
+    for(j = 0; j < 16; j++) {
+      u = (u + h[j]) | 0;
+      h[j] = u & 255;
+      u >>>= 8;
+    }
+    u = (u + h[16]) | 0; h[16] = u & 3;
+    u = (5 * (u >>> 2)) | 0;
+    for(j = 0; j < 16; j++) {
+      u = (u + h[j]) | 0;
+      h[j] = u & 255;
+      u >>>= 8;
+    }
+    u = (u + h[16]) | 0; h[16] = u;
+  }
+
+  for(j = 0; j < 17; j++) g[j] = h[j];
+  add1305(h,minusp);
+  s = (-(h[16] >>> 7) | 0);
+  for(j = 0; j < 17; j++) h[j] ^= s & (g[j] ^ h[j]);
+
+  for(j = 0; j < 16; j++) c[j] = k[j + 16];
+  c[16] = 0;
+  add1305(h,c);
+  for(j = 0; j < 16; j++) out[outpos+j] = h[j];
+}
+
+function vn(x, xpos, y, ypos, n) {
+  var i,d = 0;
+  for(i = 0; i < n; i++) d |= x[xpos+i]^y[ypos+i];
+  return (1 & ((d - 1) >>> 8)) - 1;
+}
+
+function crypto_verify_16(x, xpos, y, ypos) {
+  return vn(x,xpos,y,ypos,16);
+}
+
+function crypto_verify_32(x, xpos, y, ypos) {
+  return vn(x,xpos,y,ypos,32);
+}
+
+function crypto_onetimeauth_verify(h, hpos, m, mpos, n, k) {
+  var x = [];
+  crypto_onetimeauth(x,0,m,mpos,n,k);
+  return crypto_verify_16(h,hpos,x,0);
+}
+
+/* Secret box */
+
+function crypto_secretbox(c,m,d,n,k) {
+  var i;
+  if (d < 32) throw new Error("crypto_secretbox: d < 32");
+  crypto_stream_xor(c,0,m,0,d,n,k);
+  crypto_onetimeauth(c, 16, c, 32, d - 32, c);
+  for(i = 0; i < 16; i++) c[i] = 0;
+}
+
+function crypto_secretbox_open(m,c,d,n,k) {
+  var i;
+  var x = [];
+  if (d < 32) throw new Error("crypto_secretbox_open: d < 32");
+  crypto_stream(x,0,32,n,k);
+  if (crypto_onetimeauth_verify(c, 16,c, 32,d - 32,x) != 0) return false;
+  crypto_stream_xor(m,0,c,0,d,n,k);
+  for(i = 0; i < 32; i++) m[i] = 0;
+  return true;
+}
+
+var crypto_secretbox_KEYBYTES = 32,
+    crypto_secretbox_NONCEBYTES = 24,
+    crypto_secretbox_ZEROBYTES = 32,
+    crypto_secretbox_BOXZEROBYTES = 16;
+
+exports.crypto_stream_xor = crypto_stream_xor;
+exports.crypto_stream = crypto_stream;
+exports.crypto_stream_salsa20_xor = crypto_stream_salsa20_xor;
+exports.crypto_stream_salsa20 = crypto_stream_salsa20;
+exports.crypto_onetimeauth = crypto_onetimeauth;
+exports.crypto_onetimeauth_verify = crypto_onetimeauth_verify;
+exports.crypto_verify_16 = crypto_verify_16;
+exports.crypto_verify_32 = crypto_verify_32;
+exports.crypto_secretbox = crypto_secretbox;
+exports.crypto_secretbox_open = crypto_secretbox_open;
+exports.crypto_secretbox_KEYBYTES = crypto_secretbox_KEYBYTES;
+exports.crypto_secretbox_NONCEBYTES = crypto_secretbox_NONCEBYTES;
+exports.crypto_secretbox_ZEROBYTES = crypto_secretbox_ZEROBYTES;
+exports.crypto_secretbox_BOXZEROBYTES = crypto_secretbox_BOXZEROBYTES;
+
+/* Encodings */
+
+function bytesFromUTF8(s) {
+  var b = [], i;
+  s = unescape(encodeURIComponent(s));
+  for (i = 0; i < s.length; i++) b.push(s.charCodeAt(i));
+  return b;
+}
+
+function bytesToUTF8(b) {
+  var s = [], i;
+  for (i = 0; i < b.length; i++) s.push(String.fromCharCode(b[i]));
+  return decodeURIComponent(escape(s.join('')));
+}
+
+function getBytes(s) {
+  return (typeof s === 'string') ? bytesFromUTF8(s) : s;
+}
+
+function encodeBase64(b) {
+  var i, s = [], len = b.length;
+  if (typeof btoa === 'undefined') {
+    return (new Buffer(b)).toString('base64');
+  }
+  for (i = 0; i < len; i++) s.push(String.fromCharCode(b[i]));
+  return btoa(s.join(''));
+}
+
+function decodeBase64(s) {
+  var b = [], i;
+  if (typeof atob === 'undefined') {
+    return Array.prototype.slice.call(new Buffer(s, 'base64'), 0);
+  }
+  s = atob(s);
+  for (i = 0; i < s.length; i++) b.push(s.charCodeAt(i));
+  return b;
+}
+
+function checkLengths(k, n) {
+  if (k.length != crypto_secretbox_KEYBYTES)
+    throw new Error('bad key length');
+  if (n.length != crypto_secretbox_NONCEBYTES)
+    throw new Error('bad nonce length');
+}
+
+/* High-level API */
+exports.secretbox = exports.secretbox || {};
+
+exports.secretbox.seal = function(msg, nonce, key) {
+  var i, m = [], c = [], k, n;
+  for (i = 0; i < crypto_secretbox_ZEROBYTES; i++) m.push(0);
+  m = m.concat(getBytes(msg));
+  k = getBytes(key);
+  n = getBytes(nonce);
+  checkLengths(k, n);
+  crypto_secretbox(c, m, m.length, n, k);
+  return encodeBase64(c.slice(crypto_secretbox_BOXZEROBYTES));
+};
+
+exports.secretbox.open = function(box, nonce, key) {
+  var i, m = [], c = [], k, n;
+  for (i = 0; i < crypto_secretbox_BOXZEROBYTES; i++) c.push(0); 
+  try { c = c.concat(decodeBase64(box)); } catch(e) { return false; }
+  if (c.length < 32) return false;
+  k = getBytes(key);
+  n = getBytes(nonce);
+  checkLengths(k, n);
+  if (!crypto_secretbox_open(m, c, c.length, n, k)) return false;
+  return bytesToUTF8(m.slice(crypto_secretbox_ZEROBYTES));
+};
+
+})(typeof exports !== 'undefined' ? exports : (window.nacl = window.nacl || {}));
---
+++ test.js
@@ -0,0 +1,212 @@
+var nacl = (typeof require !== 'undefined') ? require('./nacl.js') : window.nacl;
+
+if (!nacl) throw new Error('nacl not loaded');
+
+function bytes_equal(x, y) {
+  if (x.length !== y.length) return false;
+  for (var i = 0; i < x.length; i++) {
+    if (x[i] !== y[i]) return false;
+  }
+  return true;
+}
+
+function benchmark(fn) {
+  var start = new Date(), MB = 1;
+  for (i = 0; i < MB*1024; i++) {
+    fn();
+  }
+  var elapsed = (new Date()) - start;
+  console.log('', (MB*1000)/elapsed, 'MB/s');
+}
+
+/*
+ * Test and benchmark.
+ */
+
+function crypto_stream_xor_test() {
+  console.log('Testing crypto_stream_xor');
+  var golden = [
+    {
+      m:[72,101,108,108,111,32,119,111,114,108,100,33],
+      n:[50,52,45,98,121,116,101,32,110,111,110,99,101,32,102,111,114,32,120,115,97,108,115,97],
+      k:[116,104,105,115,32,105,115,32,51,50,45,98,121,116,101,32,107,101,121,32,102,111,114,32,120,115,97,108,115,97,50,48],
+      out:[0,45,69,19,132,63,194,64,196,1,229,65]
+    },
+    {
+      m:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      n:[50,52,45,98,121,116,101,32,110,111,110,99,101,32,102,111,114,32,120,115,97,108,115,97],
+      k:[116,104,105,115,32,105,115,32,51,50,45,98,121,116,101,32,107,101,121,32,102,111,114,32,120,115,97,108,115,97,50,48],
+      out:[72,72,41,127,235,31,181,47,182,109,129,96,155,213,71,250,188,190,112,38,237,200,181,229,228,73,208,136,191,166,156,8,143,93,141,161,215,145,38,124,44,25,90,127,140,174,156,75,64,80,208,140,230,211,161,81,236,38,95,58,88,228,118,72]
+    }
+  ];
+
+  for (var i = 0; i < golden.length; i++) {
+    var out = [];
+    nacl.crypto_stream_xor(out, 0, golden[i].m, 0, golden[i].m.length, golden[i].n, golden[i].k);
+    if (!bytes_equal(out, golden[i].out)) {
+      console.log(i, 'differ');
+      console.log('expected', golden[i].out, 'got', out);
+    } else {
+      console.log(i, 'OK');
+    }
+  }
+
+}
+
+function crypto_stream_xor_benchmark() {
+  console.log('Benchmarking crypto_stream_xor');
+  var m = [], n = [], k = [], out = [];
+  for (i = 0; i < 1024; i++) m[i] = i & 255;
+  for (i = 0; i < 24; i++) n[i] = i;
+  for (i = 0; i < 32; i++) k[i] = i;
+  benchmark(function(){
+    nacl.crypto_stream_xor(out, 0, m, 0, m.length, n, k);
+  });
+}
+
+
+function crypto_onetimeauth_test() {
+  console.log('Testing crypto_onetimeauth');
+  var golden = [
+    {
+      m:[72,101,108,108,111,32,119,111,114,108,100,33],
+      k:[116,104,105,115,32,105,115,32,51,50,45,98,121,116,101,32,107,101,121,32,102,111,114,32,80,111,108,121,49,51,48,53],
+      out:[166,247,69,0,143,129,201,22,162,13,204,116,238,242,178,240]
+    },
+    {
+      m:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      k:[116,104,105,115,32,105,115,32,51,50,45,98,121,116,101,32,107,101,121,32,102,111,114,32,80,111,108,121,49,51,48,53],
+      out:[73,236,120,9,14,72,30,198,194,107,51,185,28,204,3,7]
+    },
+    {
+      m: [] /* 2007 zeros */,
+      k:[116,104,105,115,32,105,115,32,51,50,45,98,121,116,101,32,107,101,121,32,102,111,114,32,80,111,108,121,49,51,48,53],
+      out:[218,132,188,171,2,103,108,56,205,176,21,96,66,116,194,170]
+    },
+    {
+      m: [] /* 2007 zeros */,
+      k:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      out:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    }
+  ];
+  for (var i = 0; i < 2007; i++) {
+    golden[2].m.push(0);
+    golden[3].m.push(0);
+  }
+  for (var i = 0; i < golden.length; i++) {
+    var out = [];
+    nacl.crypto_onetimeauth(out, 0, golden[i].m, 0, golden[i].m.length, golden[i].k);
+    if (!bytes_equal(out, golden[i].out)) {
+      console.log(i, 'differ');
+      console.log('expected', golden[i].out, 'got', out);
+    } else {
+      console.log(i, 'OK');
+    }
+  }
+}
+
+function crypto_onetimeauth_benchmark() {
+  console.log('Benchmarking crypto_onetimeauth');
+  var m = [], out = [];
+  var k = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1];
+  for (i = 0; i < 1024; i++) {
+    m[i] = i & 255;
+  }
+  benchmark(function(){
+    nacl.crypto_onetimeauth(out, 0, m, 0, m.length, k);
+  });
+}
+
+
+function crypto_secretbox_test() {
+  console.log('Testing crypto_secretbox');
+  var i, k = [], n = [], m = [], c = [];
+  for (i = 0; i < 32; i++) k[i] = 1;
+  for (i = 0; i < 24; i++) n[i] = 2;
+  for (i = 0; i < 64; i++) m[i] = 3;
+  // Pad m.
+  for (i = 0; i < nacl.crypto_secretbox_ZEROBYTES; i++) m.unshift(0);
+
+  nacl.crypto_secretbox(c, m, m.length, n, k);
+
+  var golden = [132,66,188,49,63,70,38,241,53,158,59,80,18,43,108,230,254,102,221,254,125,57,209,78,99,126,180,253,91,69,190,173,171,85,25,141,246,171,83,104,67,151,146,162,60,135,219,112,172,182,21,109,197,239,149,122,192,79,98,118,207,96,147,184,75,231,127,240,132,156,195,62,52,183,37,77,90,143,101,173];
+
+  // Unpad c.
+  out = c.slice(nacl.crypto_secretbox_BOXZEROBYTES);
+
+  if (!bytes_equal(out, golden)) {
+    console.log(0, 'differ');
+    console.log('expected', golden, 'got', out);
+  } else {
+    console.log(0, 'OK');
+  }
+
+  // Try opening.
+  var opened = [];
+  if (!nacl.crypto_secretbox_open(opened, c, c.length, n, k)) {
+    console.log('open failed');
+  } else {
+    console.log('opened - OK');
+  }
+  if (!bytes_equal(opened, m)) {
+    console.log(1, 'differ');
+    console.log('expected', m, 'got', opened);
+  } else {
+    console.log(1, 'OK');
+  }
+}
+
+function crypto_secretbox_benchmark() {
+  console.log('Benchmarking crypto_secretbox');
+  var i, k = [], n = [], m = [], c = [];
+  for (i = 0; i < 32; i++) k[i] = 1;
+  for (i = 0; i < 24; i++) n[i] = 2;
+  for (i = 0; i < 1024; i++) m[i] = 3;
+  benchmark(function() {
+    nacl.crypto_secretbox(c, m, m.length, n, k);
+  });
+}
+
+function seal_open_test() {
+  console.log('Testing secretbox.seal and secrebox.open');
+  var key = '12345678901234567890123456789012';
+  var nonce = '123456789012345678901234';
+  var msg = 'привет!'
+  var box = nacl.secretbox.seal(msg, nonce, key);
+  var dec = nacl.secretbox.open(box, nonce, key);
+  if (msg === dec)
+    console.log('OK');
+  else
+    console.log('expected ', msg, 'got', dec);
+}
+
+function seal_open_benchmark() {
+  var key = '12345678901234567890123456789012';
+  var nonce = '123456789012345678901234';
+  var box = null;
+  var msg = '';
+  for (var i = 0; i < 1024; i++) msg += 'a';
+  console.log('Benchmarking secretbox.seal');
+  benchmark(function() {
+    box = nacl.secretbox.seal(msg, nonce, key);
+  });
+  console.log('Benchmarking secretbox.open (valid)');
+  benchmark(function() {
+    nacl.secretbox.open(box, nonce, key);
+  });
+  console.log('Benchmarking secretbox.open (invalid)');
+  box = 'A' + box.substr(1);
+  benchmark(function() {
+    nacl.secretbox.open(box, nonce, key);
+  });
+}
+
+crypto_stream_xor_test();
+crypto_onetimeauth_test();
+crypto_secretbox_test();
+seal_open_test();
+
+crypto_stream_xor_benchmark();
+crypto_onetimeauth_benchmark();
+crypto_secretbox_benchmark();
+seal_open_benchmark();
