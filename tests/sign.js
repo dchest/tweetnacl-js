3ffb11bd2e5fef987fd8f0260ba5fad6c4680c8f
Dmitry Chestnykh <dmitry@codingrobots.com>
2014-06-14 20:09:44 +0200

    Add csign test.

---
+++ tests/sign.js
@@ -0,0 +1,45 @@
+var nacl = require('../nacl.js');
+var crypto = require('crypto');
+var spawn = require('child_process').spawn;
+
+function csign(sk, msg, callback) {
+  var hexsk = (new Buffer(sk)).toString('hex');
+  var csign = spawn('./csign', [hexsk]);
+  var result = [];
+  csign.stdout.on('data', function(data) {
+    result.push(data);
+  });
+  csign.on('close', function(code) {
+    var sigFromC = Buffer.concat(result).toString('base64');
+    return callback(sigFromC);
+  });
+  csign.on('error', function(err) {
+    throw err;
+  });
+  csign.stdin.write(msg);
+  csign.stdin.end();
+}
+
+function check(i, sk, pk) {
+  var msg = crypto.randomBytes(i).toString('base64');
+  console.log("\nTest #" + i + " (Message length: " + msg.length + ")");
+  var sig = nacl.sign(msg, sk);
+  csign(sk, msg, function(sigFromC) {
+    if (sigFromC != sig) {
+      console.error("! signatures don't match\nJS: ", sig, "\nC : ", sigFromC);
+    } else {
+      console.log("sign - OK");
+    }
+    if (!nacl.sign.open(msg, sigFromC, pk)) {
+      console.log("! verification failed");
+    } else {
+      console.log("open - OK");
+    }
+    if (i == 100) { return; }
+    check(i+1, sk, pk);
+  });
+}
+
+var sk = [], pk = [];
+nacl.crypto_sign_keypair(pk, sk);
+check(0, sk, pk);
