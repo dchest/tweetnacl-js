53918bf3b0a55e3f1174e91a951a9fe4c8e0033f
Dmitry Chestnykh <dmitry@codingrobots.com>
2014-06-16 16:34:13 +0200

    sign test: stop on the first error; use "xxxx.." instead of a random message.

--- tests/sign.js
+++ tests/sign.js
@@ -21,17 +21,20 @@ function csign(sk, msg, callback) {
 }
 
 function check(i, sk, pk) {
-  var msg = crypto.randomBytes(i).toString('base64');
+  //var msg = crypto.randomBytes(i).toString('base64');
+  var msg = new Array(i).join('x');
   console.log("\nTest #" + i + " (Message length: " + msg.length + ")");
   var sig = nacl.sign(msg, sk);
   csign(sk, msg, function(sigFromC) {
     if (sigFromC != sig) {
       console.error("! signatures don't match\nJS: ", sig, "\nC : ", sigFromC);
+      process.exit(1);
     } else {
       console.log("sign - OK");
     }
     if (nacl.sign.open(msg, sigFromC, pk) === false) {
       console.log("! verification failed");
+      process.exit(1);
     } else {
       console.log("open - OK");
     }
