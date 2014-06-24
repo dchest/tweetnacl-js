1b29691e502cad4ac5a61d82bddc9717887abfb1
Dmitry Chestnykh <dmitry@codingrobots.com>
2014-06-24 19:33:01 +0200

    Shorten credits.

--- nacl.js
+++ nacl.js
@@ -1,8 +1,8 @@
 (function(exports) {
 'use strict';
 
-// Written in 2014 by Dmitry Chestnykh. Public domain.
-// Written in 2014 by Devi Mandiri. Public domain.
+// Ported in 2014 by Dmitry Chestnykh and Devi Mandiri.
+// Public domain.
 //
 // Implementation derived from TweetNaCl version 20140427.
 // See for details: http://tweetnacl.cr.yp.to/
