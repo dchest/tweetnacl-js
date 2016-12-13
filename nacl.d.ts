6523d5401b4e528fe8805a5a6f23ff2506e56d94
Danilo Bargen <mail@dbrgn.ch>
2016-12-13 10:48:21 +0100

    Typings: secretbox.open can return false

--- nacl.d.ts
+++ nacl.d.ts
@@ -18,7 +18,7 @@ declare namespace nacl {
 
     export interface secretbox {
         (msg: Uint8Array, nonce: Uint8Array, key: Uint8Array): Uint8Array;
-        open(box: Uint8Array, nonce: Uint8Array, key: Uint8Array): Uint8Array;
+        open(box: Uint8Array, nonce: Uint8Array, key: Uint8Array): Uint8Array | false;
         readonly keyLength: number;
         readonly nonceLength: number;
         readonly overheadLength: number;
@@ -33,7 +33,7 @@ declare namespace nacl {
 
     namespace box {
         export interface open {
-            (msg: Uint8Array, nonce: Uint8Array, publicKey: Uint8Array, secretKey: Uint8Array): Uint8Array;
+            (msg: Uint8Array, nonce: Uint8Array, publicKey: Uint8Array, secretKey: Uint8Array): Uint8Array | false;
             after(box: Uint8Array, nonce: Uint8Array, key: Uint8Array): Uint8Array;
         }
 
