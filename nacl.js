3908735b359aa03119f9469f53c27dd9f34f5def
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-02-22 11:22:21 +0100

    Use instanceof Uint8Array instead of toString to check for correct type
    Closes #119

--- nacl.js
+++ nacl.js
@@ -933,10 +933,9 @@ function checkBoxLengths(pk, sk) {
 }
 
 function checkArrayTypes() {
-  var t, i;
-  for (i = 0; i < arguments.length; i++) {
-     if ((t = Object.prototype.toString.call(arguments[i])) !== '[object Uint8Array]')
-       throw new TypeError('unexpected type ' + t + ', use Uint8Array');
+  for (var i = 0; i < arguments.length; i++) {
+    if (!(arguments[i] instanceof Uint8Array))
+      throw new TypeError('unexpected type, use Uint8Array');
   }
 }
 
